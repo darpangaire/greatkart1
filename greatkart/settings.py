bbe50b7a92d50de558d78965b5b605384d62fd8a
darpangaire <darpangaire11@gmail.com>
2025-05-21 09:14:25 +0545

    change url of images

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -86,16 +86,25 @@ AUTH_USER_MODEL = 'accounts.Account'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER':'postgres',
+#         'PASSWORD':'XLOwKuYkUqVyVzbbQJCSTJHqBLePIUIo',
+#         'HOST':'autorack.proxy.rlwy.net',
+#         'PORT':'46946',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER':'postgres',
-        'PASSWORD':'XLOwKuYkUqVyVzbbQJCSTJHqBLePIUIo',
-        'HOST':'autorack.proxy.rlwy.net',
-        'PORT':'46946',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
